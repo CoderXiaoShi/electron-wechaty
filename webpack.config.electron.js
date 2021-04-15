99be45bab66c7a158c91f0f7b969fd73412da78d
石兴龙 <1003865572@qq.com>
2021-04-15 17:03:00 +0800

    update

--- webpack.config.electron.js
+++ webpack.config.electron.js
@@ -4,8 +4,41 @@ module.exports = {
     entry: path.resolve(__dirname, './electron-main.js'),
     output: {
         path: path.resolve(__dirname, 'dist_electron'),
-        filename: 'built.js',
+        filename: 'main.js',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+          {
+            test: /\.node$/,
+            use: 'node-loader',
+          },
+          {
+            type: 'javascript/auto',
+            test: /\.mjs$/,
+            use: [],
+          },
+        ],
+    },
+    node: {
+      __dirname: false,
+      __filename: false,
     },
     target: 'electron-main',
     mode: 'development'
 }
+
+/*
+yarn add -D babel-loader @babel/core  @babel/preset-env
+
+
+yarn add -D babel-loader @babel/core  @babel/preset-env babel-plugin-transform-vue-jsx @babel/plugin-transform-runtime @babel/plugin-transform-modules-commonjs @babel/plugin-proposal-class-properties @babel/plugin-proposal-optional-chaining @babel/env
+
+*/
